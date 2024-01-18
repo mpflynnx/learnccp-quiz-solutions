16edf3d8cc43e5f038c9fd76ec1efd8f0a6066e0
mpflynnx <mpflynnx@gmail.com>
2024-01-18 15:35:18 +0000

    #3 wip

--- chapter4-fundamental-data-types/chapter4-quiz-question3/main.cpp
+++ chapter4-fundamental-data-types/chapter4-quiz-question3/main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include <cmath>
+
+double ballHeight(double height, double seconds)
+{
+    double newHeight{};
+    double gravity{ 9.8 };
+    newHeight = height - (0.5 * gravity * pow(seconds, 2.0));
+    return newHeight;
+}
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    std::cout << "Enter the height of the tower in meters: ";
+    double height{};
+    std::cin >> height;
+    //double seconds{};
+
+    
+    if (ballHeight(100.0, 1.0) > 0)
+        std::cout << "At 1 seconds, the ball is at height: " << ballHeight(100.0, 1.0) << " meters\n";
+        std::cout << "At 2 seconds, the ball is at height: " << ballHeight(100.0, 2.0) << " meters\n";
+        std::cout << "At 3 seconds, the ball is at height: " << ballHeight(100.0, 3.0) << " meters\n";
+        std::cout << "At 4 seconds, the ball is at height: " << ballHeight(100.0, 4.0) << " meters\n";
+        std::cout << "At 5 seconds, the ball is at height: " << ballHeight(100.0, 5.0) << " meters\n";
+ /*   else
+        std::cout << "At 1 seconds, the ball is ";
+        std::cout << "on the ground.";*/
+
     return 0;
 }
 
