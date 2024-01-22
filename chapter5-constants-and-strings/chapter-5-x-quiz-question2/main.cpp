46e75f419fe31e9cd7a33dafb8a93c5c2edd4880
mpflynnx <mpflynnx@gmail.com>
2024-01-22 13:56:28 +0000

    #7 Initial code with issues Find 3 issues (affecting 4 lines) in the following code.

---
+++ chapter5-constants-and-strings/chapter-5-x-quiz-question2/main.cpp
@@ -0,0 +1,21 @@
+#include <cstdint> // for std::uint8_t
+#include <iostream>
+
+int main()
+{
+    std::cout << "How old are you?\n";
+
+    std::uint8_t age{};
+    std::cin >> age;
+
+    std::cout << "Allowed to drive a car in Texas [";
+
+    if (age >= 16)
+        std::cout << "x";
+    else
+        std::cout << ' ';
+
+    std::cout << "]\n";
+
+    return 0;
+}
