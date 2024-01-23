68ba8af9c5e280f474769edd95280dfae2a23333
mpflynnx <mpflynnx@gmail.com>
2024-01-23 11:04:16 +0000

    #13 My solution.

--- chapter5-constants-and-strings/chapter5x-quiz-question6/main.cpp
+++ chapter5-constants-and-strings/chapter5x-quiz-question6/main.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
 
-// Write the function getQuantityPhrase() here
+constexpr std::string_view getQuantityPhrase(int x)
+{
+    if (x < 0)
+        return "negative";
+    else
+        switch (x)
+        {
+        case 0:
+            return "no";
+        case 1:
+            return "a single";
+        case 2:
+            return "a couple of";
+        case 3:
+            return "a few";
+        default:
+            return "many";
+        }
 
-// Write the function getApplesPluralized() here
+}
+
+std::string getApplesPluralized(int x)
+{
+    return (x == 1 ? "apple" : "apples");
+}
 
 int main()
 {
-    constexpr int maryApples{ 3 };
+    constexpr int maryApples{ 2 };
     std::cout << "Mary has " << getQuantityPhrase(maryApples) << ' ' << getApplesPluralized(maryApples) << ".\n";
 
     std::cout << "How many apples do you have? ";
--- chapter5-constants-and-strings/chapter5x-quiz-question6/question6.md
+++ chapter5-constants-and-strings/chapter5x-quiz-question6/question6.md
@@ -34,17 +34,20 @@ You have a single apple.
 
 getQuantityPhrase() should take a single int parameter representing the quantity of something and return the following descriptor:
 
-< 0 = “negative”
-0 = “no”
-1 = “a single”
-2 = “a couple of”
-3 = “a few”
-> 3 = “many”
+- < 0 = “negative”
+- 0 = “no”
+- 1 = “a single”
+- 2 = “a couple of”
+- 3 = “a few”
+- \> 3 = “many”
 
 getApplesPlural() should take a single int parameter parameter representing the quantity of apples and return the following:
 
-1 = “apple”
-otherwise = “apples”
+- 1 = “apple”
+- otherwise = “apples”
+
 This function should use the conditional operator.
 
 Both functions should make proper use of constexpr.
+
+Hint: It’s okay to return a C-style string literal from a function as a std::string_view.
