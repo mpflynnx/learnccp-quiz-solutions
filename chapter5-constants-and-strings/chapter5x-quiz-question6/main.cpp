7cd1530e99822ea49e54ef9bc93074184ea40d44
mpflynnx <mpflynnx@gmail.com>
2024-01-23 10:22:12 +0000

    #13 Add starting program Program requires completion.

--- chapter5-constants-and-strings/chapter5x-quiz-question6/main.cpp
+++ chapter5-constants-and-strings/chapter5x-quiz-question6/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 
+// Write the function getQuantityPhrase() here
+
+// Write the function getApplesPluralized() here
+
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    constexpr int maryApples{ 3 };
+    std::cout << "Mary has " << getQuantityPhrase(maryApples) << ' ' << getApplesPluralized(maryApples) << ".\n";
+
+    std::cout << "How many apples do you have? ";
+    int numApples{};
+    std::cin >> numApples;
+
+    std::cout << "You have " << getQuantityPhrase(numApples) << ' ' << getApplesPluralized(numApples) << ".\n";
+
     return 0;
 }
