2626ad52ec913867ec6ed9c162975d3a3c44e045
mpflynnx <mpflynnx@gmail.com>
2024-01-24 11:32:24 +0000

    #17 Solution

--- chaper0-bit-manipulation/chapter0-2-quiz-question3/main.cpp
+++ chaper0-bit-manipulation/chapter0-2-quiz-question3/main.cpp
@@ -3,23 +3,24 @@
 
 std::bitset<4> rotl(std::bitset<4> bits)
 {
-	const bool leftbit{ bits.test(3) };
+	// bits << 1 does the left shift
+	// bits >> 3 handle the rotation of the leftmost bit
+														   // 0011 START
 
-	bits <<= 1; // do left shift
-
-	if (leftbit)
-		bits.set(0);
-
-	return bits;
+	std::cout << bits << " << 1: " << (bits << 1) << '\n'; // 0010 OR
+	std::cout << bits << " >> 3: " << (bits >> 3) << '\n'; // 0001
+													       // ----
+	                                                       // 0011
+	return (bits << 1) | (bits >> 3);
 }
 
 int main()
 {
 	std::bitset<4> bits1{ 0b0001 };
-	std::cout << rotl(bits1) << '\n';
+	std::cout << rotl(bits1) << '\n'; // 0010
 
 	std::bitset<4> bits2{ 0b1001 };
-	std::cout << rotl(bits2) << '\n';
+	std::cout << rotl(bits2) << '\n'; // 0011
 
 	return 0;
 }
