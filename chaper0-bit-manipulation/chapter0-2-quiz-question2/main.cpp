45a16542638fb56e45e90cc80abcbc5f95bf08f2
mpflynnx <mpflynnx@gmail.com>
2024-01-24 10:47:08 +0000

    #15 solution

--- chaper0-bit-manipulation/chapter0-2-quiz-question2/main.cpp
+++ chaper0-bit-manipulation/chapter0-2-quiz-question2/main.cpp
@@ -4,16 +4,23 @@
 // "rotl" stands for "rotate left"
 std::bitset<4> rotl(std::bitset<4> bits)
 {
-	// Your code here
+	const bool leftbit{ bits.test(3) };
+
+	bits <<= 1; // do left shift
+
+	if (leftbit)
+		bits.set(0);
+
+	return bits;
 }
 
 int main()
 {
 	std::bitset<4> bits1{ 0b0001 };
-	std::cout << rotl(bits1) << '\n';
+	std::cout << rotl(bits1) << '\n'; // 0010
 
 	std::bitset<4> bits2{ 0b1001 };
-	std::cout << rotl(bits2) << '\n';
+	std::cout << rotl(bits2) << '\n'; // 0011
 
 	return 0;
 }
