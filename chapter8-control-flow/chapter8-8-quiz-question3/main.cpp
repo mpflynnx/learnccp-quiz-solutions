03997de85804e9f1c1b05c3b9ed3dc6f80091415
mpflynnx <mpflynnx@gmail.com>
2024-02-01 09:12:56 +0000

    #29 My solution quiz question 3

--- chapter8-control-flow/chapter8-8-quiz-question3/main.cpp
+++ chapter8-control-flow/chapter8-8-quiz-question3/main.cpp
@@ -10,23 +10,23 @@
 
 int main()
 {
-    // outer loops between 1 and 5
-    int outer{ 1 };
-    while (outer <= 5)
+    // outer loops between 5 and 1
+    int outer{ 5 };
+    while (outer >= 1)
     {
         // For each iteration of the outer loop, the code in the body of the loop executes once
 
-        // inner loops between 1 and outer
-        int inner{ 1 };
-        while (inner <= outer)
+        // inner loops between outer and 1
+        int inner{ outer }; 
+        while (inner >= 1)
         {
             std::cout << inner << ' ';
-            ++inner;
+            --inner;
         }
 
         // print a newline at the end of each row
         std::cout << '\n';
-        ++outer;
+        --outer;
     }
 
     return 0;
