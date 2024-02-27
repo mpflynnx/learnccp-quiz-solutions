33ebe6c3bcd96a123c33cdc9ba4af8d52e913baf
mpflynnx <mpflynnx@gmail.com>
2024-02-27 10:11:04 +0000

    19.2 Dynamically allocating arrays Quiz Question #1 #33 refactor

--- chapter19-dynamic-allocation/chapter19-2-quiz-question1/main.cpp
+++ chapter19-dynamic-allocation/chapter19-2-quiz-question1/main.cpp
@@ -2,30 +2,41 @@
 #include <string>
 #include <algorithm> // for std::sort
 
+std::size_t getLength();
+void getNames(std::string* array, std::size_t length);
 void display(const std::string* array, size_t size);
 
 int main()
 {
-    std::cout << "How many names would you like to enter? ";
 
+    std::size_t length{ getLength()};
 
-    std::size_t length{};
-    std::cin >> length;
-
+    // Allocate an array to hold the names
     std::string* array{ new std::string[length] {} };
 
-    for (size_t i{0}; i < length; ++i) {
-        std::cout << "Enter name #" << (i + 1) << ": ";
-        std::string name{};
-        std::getline(std::cin >> std::ws, name); // read a full line of text into name
-        array[i] = name;
-    }
+    getNames(array, length);
 
     std::sort(array, array + length);
     display(array, length);
 
     delete[] array; // use array delete to deallocate array
     return 0;
+}
+
+std::size_t getLength()
+{
+    std::cout << "How many names would you like to enter? ";
+    std::size_t length{};
+    std::cin >> length;
+
+    return length;
+}
+
+void getNames(std::string* array, std::size_t length) {
+    for (size_t i{ 0 }; i < length; ++i) {
+        std::cout << "Enter name #" << (i + 1) << ": ";
+        std::getline(std::cin >> std::ws, array[i]); // read a full line of text into name
+    }
 }
 
 void display(const std::string* array, size_t size) {
