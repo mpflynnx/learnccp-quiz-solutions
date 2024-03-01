c8fc8acabed34fe1229fb89ae49e932297d0b19a
mpflynnx <mpflynnx@gmail.com>
2024-03-01 14:48:22 +0000

    Passing and returning structs quizzes #37 my solution for quiz question#2

--- chapter13-compound-types-enums-structs/chapter-13-8-quiz-question2/main.cpp
+++ chapter13-compound-types-enums-structs/chapter-13-8-quiz-question2/main.cpp
@@ -1,8 +1,46 @@
 #include <iostream>
 
-int main()
+// Create a struct to hold a fraction.
+struct Fraction
 {
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+    int numerator{};
+    int denominator{};
+};
+
+// Write a function to read in a Fraction from the user
+
+Fraction getFraction() {
+
+    Fraction temp{};
+    std::cout << "Enter a value for the numerator: ";
+    std::cin >> temp.numerator;
+    std::cout << "Enter a value for the denominator: ";
+    std::cin >> temp.denominator;
+
+    return temp;
 }
 
+//Write another function to multiply two Fractions together
+Fraction multipleFraction(const Fraction& fract1, const Fraction& fract2) {
+    Fraction temp{};
+    temp.numerator = fract1.numerator * fract2.numerator;
+    temp.denominator = fract1.denominator * fract2.denominator;
+
+    return temp;
+}
+
+// Write another function that prints a fraction
+void printFraction(const Fraction& fract) {
+
+    std::cout << "Your fractions multiplied together: " << fract.numerator << '/' << fract.denominator << '\n';
+}
+
+int main()
+{
+    Fraction fract1{ getFraction() };
+    Fraction fract2{ getFraction() };
+    Fraction result{ multipleFraction(fract1, fract2) };
+    printFraction(result);
+
+    return 0;
+}
